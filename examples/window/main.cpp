294ea96e3e9c69dcaa1ac39c4702cd406dbcb624
homma <dskhomma@gmail.com>
2025-11-29 23:02:27 +0900

    add centralize function

--- examples/window/main.cpp
+++ examples/window/main.cpp
@@ -1,6 +1,19 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 
+auto centralize(Fl_Window &win) -> void {
+  auto window_width = win.w();
+  auto window_height = win.h();
+
+  auto screen_width = Fl::w();
+  auto screen_height = Fl::h();
+
+  auto top = (screen_height - window_height) / 2.0;
+  auto left = (screen_width - window_width) / 2.0;
+
+  win.position(left, top);
+}
+
 auto main() -> int {
 
   auto width = 640;
@@ -12,6 +25,7 @@ auto main() -> int {
 
   win.end();
   win.show();
+  centralize(win);
 
   return Fl::run();
 }
