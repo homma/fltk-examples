80f1de6a631266e6ab32d2858441999ac01f6334
homma <dskhomma@gmail.com>
2025-12-07 01:57:33 +0900

    add a repeat button example

---
+++ examples/repeat_button/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm fltk_repeat_button
---
+++ examples/repeat_button/main.cpp
@@ -0,0 +1,42 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Repeat_Button.H>
+#include <FL/Fl_Window.H>
+#include <print>
+
+auto main() -> int {
+
+  Fl_Window *win;
+  {
+    auto width = 640;
+    auto height = 480;
+    auto left = (Fl::w() - width) / 2.0;
+    auto top = (Fl::h() - height) / 2.0;
+    auto title = "My FLTK Window";
+    win = new Fl_Window(left, top, width, height, title);
+  }
+
+  win->color(54);
+
+  Fl_Button *button;
+  {
+    auto width = 300;
+    auto height = 40;
+    auto left = (win->w() - width) / 2.0;
+    auto top = win->h() / 2.0 - height;
+    auto text = "リピートボタン";
+    button = new Fl_Repeat_Button(left, top, width, height, text);
+  }
+
+  button->type(FL_NORMAL_BUTTON);
+  button->color(FL_WHITE);
+
+  button->callback([](Fl_Widget *w) {
+    auto value = Fl::event_button();
+    std::println("Button Event with Value: {}.", value);
+  });
+
+  win->end();
+  win->show();
+
+  return Fl::run();
+}
